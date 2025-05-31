95f9f8dd402fd2d67f069eb70cb76b70883b1267
taufinaiden <taufin@aidencreative.com>
2025-05-31 17:47:53 +0800

    show the redis configuration

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const redisPublisher = redisClient.duplicate();
 // Express route handlers
 
 app.get('/', (req, res) => {
-  res.send('Hi');
+  res.send('Hi. <br>' + keys.redisHost + '<br>' + keys.pgHost);
 });
 
 app.get('/values/all', async (req, res) => {
